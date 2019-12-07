eb6ec2d4f5202d4b57acd1456ed09900b7049ede
Rory Jacob <roryjacob@gmail.com>
2019-12-07 12:32:38 -0800

    complete day2 part 2

--- day2.kt
+++ day2.kt
@@ -6,6 +6,18 @@ fun main(args: Array<String>) {
     val instructions = ArrayList<Int>()
     input.split(",").forEach { instructions.add(it.toInt()) }
 
+    println("***********")
+    println("Part 1")
+    println("***********")
+    part1(instructions)
+    println()
+    println("***********")
+    println("Part 2")
+    println("***********")
+    part2(instructions)
+}
+
+fun part1(instructions: ArrayList<Int>) {
 //    Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input)
 //    to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running
 //    the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left
@@ -13,13 +25,40 @@ fun main(args: Array<String>) {
     instructions.set(1, 12)
     instructions.set(2, 2)
 
-    println("starting instructions:")
-    println(instructions)
-
     val result = runComputer(instructions)
 
-    println("final instruction values:")
-    println(result)
+    println("Position 0 Value: ${result.get(0)}")
+}
+
+fun part2(instructions: ArrayList<Int>) {
+    var noun = 0
+    var verb = 0
+    val expectedResult = 19690720
+    var found = false
+
+    for (i in 0..100) {
+        for (j in 0..100) {
+            instructions.set(1, i)
+            instructions.set(2, j)
+            val result = runComputer(instructions)
+
+//            println("Noun=$i Verb=$j -> ${result.get(0)}")
+
+            if (expectedResult == result.get(0)) {
+                noun = i
+                verb = j
+                println("found noun/verb combination to equal $expectedResult. noun=$noun verb=$verb")
+                found = true
+                break
+            }
+        }
+        if (found) {
+            break
+        }
+    }
+
+    val result = 100 * noun + verb
+    println("Result: $result")
 }
 
 fun runComputer(inputInstructions: ArrayList<Int>): List<Int> {
@@ -32,56 +71,56 @@ fun runComputer(inputInstructions: ArrayList<Int>): List<Int> {
     var keepTheDreamAlive = true
     while (keepTheDreamAlive and (programCounter < instructions.size)) {
         val instruction = instructions.get(programCounter)
-        println("processing instruction #${programCounter}: ${instruction}")
+//        println("processing instruction #${programCounter}: ${instruction}")
 
         when (instruction) {
             1 -> {
-                println("opcode 1 found. starting add instruction")
+//                println("opcode 1 found. starting add instruction")
 
                 val addressA = instructions.get(programCounter+1)
                 val addressB = instructions.get(programCounter+2)
                 val storeLocation = instructions.get(programCounter+3)
 
-                println("found instruction: ADD &$addressA &$addressB -> &$storeLocation")
+//                println("found instruction: ADD &$addressA &$addressB -> &$storeLocation")
                 val aValue = instructions.get(addressA)
                 val bValue = instructions.get(addressB)
 
-                println("resolved instruction: $aValue + $bValue")
+//                println("resolved instruction: $aValue + $bValue")
                 val result = aValue + bValue
 
-                println("got result $result. Storing in location $storeLocation")
+//                println("got result $result. Storing in location $storeLocation")
                 val previousValue = instructions.get(storeLocation)
                 instructions.set(storeLocation, result)
                 val newValue = instructions.get(storeLocation)
-                println("prev: $previousValue expected: $result actual: $newValue")
+//                println("prev: $previousValue expected: $result actual: $newValue")
 
-                println("add instruction complete")
+//                println("add instruction complete")
                 programCounter += 4
             }
             2 -> {
-                println("opcode 2 found. starting multiply instruction")
+//                println("opcode 2 found. starting multiply instruction")
                 val addressA = instructions.get(programCounter+1)
                 val addressB = instructions.get(programCounter+2)
                 val storeLocation = instructions.get(programCounter+3)
 
-                println("found instruction: MULTIPLY &$addressA &$addressB -> &$storeLocation")
+//                println("found instruction: MULTIPLY &$addressA &$addressB -> &$storeLocation")
                 val aValue = instructions.get(addressA)
                 val bValue = instructions.get(addressB)
 
-                println("resolved instruction: $aValue * $bValue")
+//                println("resolved instruction: $aValue * $bValue")
                 val result = aValue * bValue
 
-                println("got result $result. Storing in location $storeLocation")
+//                println("got result $result. Storing in location $storeLocation")
                 val previousValue = instructions.get(storeLocation)
                 instructions.set(storeLocation, result)
                 val newValue = instructions.get(storeLocation)
-                println("prev: $previousValue expected: $result actual: $newValue")
+//                println("prev: $previousValue expected: $result actual: $newValue")
 
-                println("multiply instruction complete")
+//                println("multiply instruction complete")
                 programCounter += 4
             }
             99 -> {
-                println("opcode 99 found. stopping processing")
+//                println("opcode 99 found. stopping processing")
                 programCounter += 1
                 keepTheDreamAlive = false
             }
